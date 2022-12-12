20d19505aad0120559a08a8dc8fc46505e5c45b7
yal1412 <catrinas877@gmail.com>
2022-12-12 23:26:54 +0300

    seq

--- cw2/src/main/java/Parallel.java
+++ cw2/src/main/java/Parallel.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Parallel {
 
@@ -15,45 +18,96 @@ public class Parallel {
     }
 
     private int degree(Integer i) {
-        return 0;
+        return graph.getVertexNeighbours(i).size();
     }
 
-    public void bfs(int startVertex) {
+    public int[] bfs(int startVertex) {
 
-        List<Integer> frontier = new ArrayList<>();   //поправить
-        AtomicBoolean[] visited = new AtomicBoolean[100];  //по количеству вершин
+        int[] frontier = new int[1];
+        AtomicBoolean[] visited = new AtomicBoolean[graph.getSize()];
+        visited[startVertex] = new AtomicBoolean(true);
+        IntStream.range(startVertex + 1, visited.length).parallel().forEach(i ->
+                visited[i] = new AtomicBoolean(false)
+        );
 
-        frontier.add(startVertex);
+        int[] distances = new int[graph.getSize()];
+        Arrays.fill(distances, -1);
+        distances[startVertex] = 0;
 
-        while (frontier.size() != 0) {
-            int[] d = new int[frontier.size()];
-            List<Integer> finalFrontier1 = frontier;
-            IntStream.range(0, frontier.size()).forEach(i ->
-                            d[i] = degree(finalFrontier1.get(i))
+        frontier[0] = startVertex;
+
+        while (frontier.length != 0) {
+            int[] d = new int[frontier.length + 1];
+            d[0] = 0;
+            int[] finalFrontier1 = frontier;
+            IntStream.range(0, frontier.length).parallel().forEach(i ->
+                            d[i + 1] = degree(finalFrontier1[i])
             );
 
             Arrays.parallelPrefix(d, Integer::sum);
-            List<Integer> frontier_p = new ArrayList<>(d[d.length - 1]);
+            Integer[] frontier_p = new Integer[d[d.length - 1]];
 
-            List<List<Integer>> neighbours = new ArrayList<>(frontier.size());
-            List<Integer> finalFrontier = frontier;
-            IntStream.range(0, frontier.size()).forEach(i -> {
-                List<Integer> N = graph.getVertexNeighbours(finalFrontier.get(i));
-                neighbours.set(i, N);
-                IntStream.range(0, degree(finalFrontier.get(i))).forEach(j -> {
-                            if (visited[neighbours.get(i).get(j)].compareAndExchange(false, true)) {
-                                frontier_p.set(d[i] + j, neighbours.get(i).get(j));
+            int[][] neighbours = new int[frontier.length][];
+            int[] finalFrontier = frontier;
+            IntStream.range(0, frontier.length).parallel().forEach(i -> {
+//                System.out.println(i + " " + Thread.currentThread().getName());
+                List<Integer> N = graph.getVertexNeighbours(finalFrontier[i]);
+                neighbours[i] = N.stream().mapToInt(Integer::intValue).toArray();
+                IntStream.range(0, degree(finalFrontier[i])).parallel().forEach(j -> {
+                            if (visited[neighbours[i][j]].compareAndSet(false, true)) {
+                                frontier_p[d[i] + j] = neighbours[i][j];
+                                distances[neighbours[i][j]] = distances[finalFrontier[i]] + 1;
                             }
                         }
                     );
                     }
             );
-            frontier = frontier_p.parallelStream().filter(Objects::nonNull).collect(Collectors.toList());
+
+            List<Integer> s = Arrays.asList(frontier_p);
+            Stream<Integer> a = s.parallelStream().filter(Objects::nonNull);
+            frontier = a.mapToInt(Integer::intValue).toArray();
+//            frontier = Arrays.asList(frontier_p).parallelStream().filter(Objects::nonNull).collect(Collectors.toList());
+        }
+
+        System.out.println(visited.length);
+        return distances;
+    }
+
+    static public void testBFS() {
+        int side = 10;
+        Graph graph = new Cube(side);
+        Parallel s = new Parallel(graph);
+        int[] d = s.bfs(0);
+        Vertex v;
+        for (int i = 0; i < graph.getSize(); i++) {
+            v = new Vertex(i, side);
+            if (d[i] != v.getI() + v.getJ() + v.getK()) {
+                System.out.println("Error in " + i + " vertex\n");
+                return;
+            }
+        }
+        System.out.println("Correct!");
+    }
+
+    static public void warmUp() {
+        int side = 100;
+        Graph graph = new Cube(side);
+        Parallel s = new Parallel(graph);
+        for (int i = 0; i < 5; i++) {
+            s.bfs(0);
         }
     }
 
     public static void main(String[] args) {
-        Graph graph = new Cube(2);
+        testBFS();
+
+        warmUp();
+
+        Graph graph = new Cube(500);
         Parallel p = new Parallel(graph);
+
+        long time = System.currentTimeMillis();
+        p.bfs(0);
+        System.out.println((System.currentTimeMillis() - time));
     }
 }
--- cw2/src/main/java/Sequential.java
+++ cw2/src/main/java/Sequential.java
@@ -1,64 +1,151 @@
-// Java program to print BFS traversal from a given source vertex.
-// BFS(int s) traverses vertices reachable from s.
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-// This class represents a directed graph using adjacency list
-// representation
 class Sequential
 {
-    private int numberOfVertex; // No. of vertices
+    private int numberOfVertex;
     private Graph graph;
 
-    // Constructor
     Sequential(Graph graph)
     {
         this.graph = graph;
         this.numberOfVertex = graph.getSize();
     }
 
-    // prints BFS traversal from a given source s
-    void BFS(int startVertex)
+    int[] BFS(int startVertex)
     {
-        // Mark all the vertices as not visited(By default
-        // set as false)
         boolean[] visited = new boolean[numberOfVertex];
 
-        // Create a queue for BFS
         LinkedList<Integer> vertexQueue = new LinkedList<Integer>();
 
-        // Mark the current node as visited and enqueue it
         visited[startVertex] = true;
         vertexQueue.add(startVertex);
 
+        int[] distances = new int[numberOfVertex];
+        Arrays.fill(distances, -1);
+        distances[startVertex] = 0;
+
         while (vertexQueue.size() != 0)
         {
-            // Dequeue a vertex from queue and print it
             startVertex = vertexQueue.poll();
 
-            // Get all adjacent vertices of the dequeued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-
-            // Получить всех соседей вершины, если в них еще не были, пометить их и добавить в очередь
             List<Integer> adjacencyList = graph.getVertexNeighbours(startVertex);
             for (Integer vertex : adjacencyList) {
                 if (!visited[vertex]) {
                     visited[vertex] = true;
                     vertexQueue.add(vertex);
+                    distances[vertex] = distances[startVertex] + 1;
                 }
             }
         }
+
+        return distances;
     }
 
-    // Driver method to
+    private int degree(Integer i) {
+        return graph.getVertexNeighbours(i).size();
+    }
+
+    public int[] bfs(int startVertex) {
+
+        int[] frontier = new int[1];
+        AtomicBoolean[] visited = new AtomicBoolean[graph.getSize()];
+        visited[startVertex] = new AtomicBoolean(true);
+        for (int i = startVertex + 1; i < visited.length; i++) {
+            visited[i] = new AtomicBoolean(false);
+        }
+//        IntStream.range(startVertex + 1, visited.length).parallel().forEach(i ->
+//                visited[i] = new AtomicBoolean(false)
+//        );
+
+        int[] distances = new int[graph.getSize()];
+        Arrays.fill(distances, -1);
+        distances[startVertex] = 0;
+
+        frontier[0] = startVertex;
+
+        while (frontier.length != 0) {
+            int[] d = new int[frontier.length + 1];
+            d[0] = 0;
+            int[] finalFrontier1 = frontier;
+//            IntStream.range(0, frontier.length).parallel().forEach(i ->
+//                    d[i + 1] = degree(finalFrontier1[i])
+//            );
+            for (int i = 0; i < frontier.length; i++) {
+                d[i + 1] = degree(finalFrontier1[i]);
+            }
+
+            for (int i = 0; i < d.length - 1; i++){
+                d[i + 1] += d[i];
+            }
+            Integer[] frontier_p = new Integer[d[d.length - 1]];
+
+            int[][] neighbours = new int[frontier.length][];
+            int[] finalFrontier = frontier;
+            IntStream.range(0, frontier.length).forEach(i -> {
+                        List<Integer> N = graph.getVertexNeighbours(finalFrontier[i]);
+                        neighbours[i] = N.stream().mapToInt(Integer::intValue).toArray();
+                        IntStream.range(0, degree(finalFrontier[i])).forEach(j -> {
+                                    if (visited[neighbours[i][j]].compareAndSet(false, true)) {
+                                        frontier_p[d[i] + j] = neighbours[i][j];
+                                        distances[neighbours[i][j]] = distances[finalFrontier[i]] + 1;
+                                    }
+                                }
+                        );
+                    }
+            );
+
+            List<Integer> s = Arrays.asList(frontier_p);
+            Stream<Integer> a = s.stream().filter(Objects::nonNull);
+            frontier = a.mapToInt(Integer::intValue).toArray();
+//            frontier = Arrays.asList(frontier_p).parallelStream().filter(Objects::nonNull).collect(Collectors.toList());
+        }
+
+        System.out.println(visited.length);
+        return distances;
+    }
+
+    static public void testBFS() {
+        int side = 4;
+        Graph graph = new Cube(side);
+        Sequential s = new Sequential(graph);
+        int[] d = s.BFS(0);
+        Vertex v;
+        for (int i = 0; i < graph.getSize(); i++) {
+            v = new Vertex(i, side);
+            if (d[i] != v.getI() + v.getJ() + v.getK()) {
+                System.out.println("Error in " + i + " vertex\n");
+                return;
+            }
+        }
+        System.out.println("Correct!");
+    }
+
+    static public void warmUp() {
+        int side = 100;
+        Graph graph = new Cube(side);
+        Sequential s = new Sequential(graph);
+        for (int i = 0; i < 5; i++) {
+            s.BFS(0);
+        }
+    }
+
+
     public static void main(String args[])
     {
+        testBFS();
+
+        warmUp();
+
         Graph graph = new Cube(500);
         Sequential s = new Sequential(graph);
 
         // ПОПРАВИТЬ НА НАНО
         long time = System.currentTimeMillis();
-        s.BFS(0);
+        int[] d = s.bfs(0);
         System.out.println((System.currentTimeMillis() - time));
+//        System.out.println(Arrays.toString(d));
     }
 }
